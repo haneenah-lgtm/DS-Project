c7f029ae3b41fb49cedc3c724228c091a9291493
Haneena <haneenahani279@gmail.com>
2025-12-12 10:55:14 +0530

    work.modi

--- work.py
+++ work.py
@@ -4,3 +4,24 @@ def add(x,y):
 
 def sub(x,y):
     return x-y
+
+def add(x,y):
+    return x+y
+
+def add(x,y):
+    return x+y
+
+def add(x,y):
+    return x+y
+
+def add(x,y):
+    return x+y
+
+def add(x,y):
+    return x+y
+
+def sub(x,y):
+    return x-y
+
+def mul(x,y):
+    return x*y
